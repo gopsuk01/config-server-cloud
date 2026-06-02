73ccf6eefb94005f73f7ac66b39f03959c71f886
ACER <htgopalsukhmai@gmail.com>
2026-06-02 08:38:16 +0530

    first commit

---
+++ src/test/java/com/gp/config_server_cloud/ConfigServerCloudApplicationTests.java
@@ -0,0 +1,14 @@
+package com.gp.config_server_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Run.....");
+	}
+
+}
